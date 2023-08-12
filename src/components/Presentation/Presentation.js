b4d064e6c3c1ea3fb0390a35b2377e0942812536
felipe.bustosp@gmail.com <felipe.bustosp@gmail.com>
2023-08-11 20:22:05 -0400

    Adding images and defining style for presentation page

--- src/components/Presentation/Presentation.js
+++ src/components/Presentation/Presentation.js
@@ -1,17 +1,23 @@
 import React from "react";
 import data from '../../resources/presentation.env.json';
+import image1 from '../../img/image_1.png';
 
 import './presentation.css';
 
 const Presentation = () => {
     return (
-        <section id="presentation">
-            <h1>{data["presentation.title.name.text"]}</h1>
-            <h2>{data["presentation.title.degree.text"]}</h2>
-            <p>{data["presentation.title.description.text.1"]}</p>
-            <p>{data["presentation.title.description.text.2"]}</p>
-            <p>{data["presentation.title.description.text.3"]}</p>
-        </section>
+        <div id="presentation">
+            <div id="presentation-img">
+                <img src={image1} alt=""/>
+            </div>
+            <div id="presentation-container">                   
+                    <h1>{data["presentation.title.name.text"]}</h1>
+                    <h2>{data["presentation.title.degree.text"]}</h2>
+                    <p>{data["presentation.title.description.text.1"]}</p>
+                    <p>{data["presentation.title.description.text.2"]}</p>
+                </div>
+            
+        </div>
     );
 };
 
